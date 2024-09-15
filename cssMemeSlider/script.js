2ffd91e7b8448a093049032c124d1b94b5da3b06
Alina Fedorova <sapereaude.alf@gmail.com>
2024-09-15 21:17:18 +0500

    feat: add js (Sun, Sep 15, 2024 09:17:18 PM)

---
+++ cssMemeSlider/script.js
@@ -0,0 +1,11 @@
+
+console.log("djjsfs")
+// element1 = document.querySelector('.e1');
+ element1 =document.getElementById("eb2");;
+element1.addEventListener('click', () => {
+    // document.getElementById('text').classList.add('bgr');
+    document.querySelector('.imgE1').classList.remove('visible');
+    document.querySelector('.imgE1').classList.add('invisible');
+    document.querySelector('.imgE2').classList.add('visible');
+
+  });
